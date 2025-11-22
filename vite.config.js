17de0b371192b470aef4fe64afc7519b05ec1af4
Gökay Şendil <gokaysendilw@outlook.com>
2025-11-22 17:11:18 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ||"/dijikor",
-  plugins: [
-    react({
-      
-      babel: {
-        include: /\.(jsx?|tsx?)$/,
-        plugins: [['babel-plugin-react-compiler']],
-        
-      },
-    }),
-  ],
+  plugins: [preact()],
 })
