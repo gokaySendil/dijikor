307eee77f9ffa2d0f3619a9acefe09c178f20c6e
Gökay Şendil <gokaysendilw@outlook.com>
2025-11-22 16:51:36 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: process.env.VITE_BASE_PATH ||"/"
+        base: process.env.VITE_BASE_PATH ||"/dijikor"
       },
     }),
   ],
